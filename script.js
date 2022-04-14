655330eb356e64f4ce22f7656a9e62a9ee806175
Rafael Aiquel Garbarini <rafael.aiquel@gmail.com>
2022-04-14 15:43:16 -0400

    Added comments to script.js - Added alert if principal is less than or equal to 0

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+// Running update rate al load of page to show the real value of the slider
+window.onload = updateRate;
+
 function compute()
 {
+    // reading values of the form into variables
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 
+    if(principal.value <= 0){
+        alert("Please enter a positive number");
+        principal.focus();
+        return false;
+    }
+
+    // Compute the interest earned in the period given
     var interest = principal * years * rate / 100;
 
+    // Adding the actual year plus the period to get the year in the future
     var year = new Date().getFullYear() + parseInt(years);
 
+    // Generate the output text with the highlighting required by the customer
     document.getElementById("result").innerHTML = 
         "If you deposit <mark>" + principal + "</mark>,<br>" +
         "at an interest rate of <mark>" + rate + "%</mark>.<br>" +
@@ -15,10 +28,9 @@ function compute()
         "in the year <mark>" + year + "</mark>";
 }
 
-
+// Updates de value of the text besides the slider
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval;
 }
-        
