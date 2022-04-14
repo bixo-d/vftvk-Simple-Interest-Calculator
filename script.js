3d10a4d3cc6cd833dd3ccbb7c95fbc688ba810d6
Rafael Aiquel Garbarini <rafael.aiquel@gmail.com>
2022-04-14 09:09:11 -0400

    Reverting modifications to script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function compute()
     document.getElementById("result").innerHTML = 
         "If you deposit " + principal + ",<br>" +
         "at an interest rate of " + rate + "%.<br>" +
-        "You will receive an amount of " + (+principal + interest) + ",<br>" +
+        "You will receive an amount of " + interest + ",<br>" +
         "in the year " + year;
 }
 
